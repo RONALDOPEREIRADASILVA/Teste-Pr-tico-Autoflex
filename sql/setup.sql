cd250c3e9d6f6fe1ac910d4b699cdff4f0636fc8
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-02-24 22:43:57 -0300

    Criei o arquivo Product.java

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,34 +1,29 @@
---Cria o Banco de Dados:
-
-CREATE DATABASE IF NOT EXISTS
-factory_db;
+--Database creation:
+CREATE DATABASE IF NOT EXISTS factory_db;
 USE factory_db;
 
---Cria tabela de  matérias-primas(Raw_materials)
-CREATE TABLE IF NOT EXISTS
-raw_materials(
+--Storing Raw_materials)
+CREATE TABLE IF NOT EXISTS raw_materials(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    stock_quantity INT NOT NULL DEFAULT 0,
-    creatd_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
---Tabela de produtos(Products)
-CREATE TABLE IF NOT EXISTS products(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHA(255) NOT NULL´
-    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL DEFAULT 0.00,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
---Tabele de Associação (Product Ingredients)
---Esta tabela liga produtos aos materiais 
-CREATE TABLE IF NOT EXISTS
-product_ingredients(
+--Storing Products information
+CREATE TABLE IF NOT EXISTS products(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    product_id INT,
-    raw_material_id INT,
-    needed_quantity INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
-    FOREIGN KEY (raw_material_id) REFERENCES raw_materials(id) ON DELETE CASCADE
+    name VARCHA(255) NOT NULL price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--Linking materials to products (Ingredients - Recipe)
+CREATE TABLE IF NOT EXISTS product_ingredients(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    raw_material_id INT NOT NULL,
+    quantity_needed DECIMAL(10, 2) NOT NULL,
+    --Foreingn keys for data integrity
+    CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+    CONSTRAINT fk_material_id FOREIGN KEY (raw_material_id) REFERENCES raw_materials(id);
 );
